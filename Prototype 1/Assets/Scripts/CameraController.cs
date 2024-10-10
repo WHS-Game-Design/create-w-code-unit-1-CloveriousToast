d9ce913459b965556848717b84f1a6116927bbfc
CloveriousToast <unlucky.clover.official@gmail.com>
2024-10-10 15:28:41 -0700

    camera follow player

--- Prototype 1/Assets/Scripts/CameraController.cs
+++ Prototype 1/Assets/Scripts/CameraController.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
-    public float speed = 10f;
+    public GameObject player;
+    public Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,8 +13,8 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.Translate(Vector3.forward*Time.deltaTime * speed);
+        transform.position = player.transform.position + offset;
     }
 }
